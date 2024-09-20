664fe5d1f5c50e999a468ffcb8d2ea9238982b39
Aaron Fleisher <amfleish@gmail.com>
2024-09-20 15:27:09 -0700

    created linked list data type, but stalled out

--- src/include/merge_two_lists.c
+++ src/include/merge_two_lists.c
@@ -1,31 +1,118 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
+struct ListNode;
+typedef struct ListNode ListNode;
+typedef struct linkedList linkedList;
+
+typedef struct linkedList {
+    uint16_t size;
+    ListNode* head;
+    ListNode* tail;
+} linkedlist;
+
 struct ListNode {
     int val;
-    struct ListNode *next;
+    ListNode *next;
 }; 
 
-typedef struct ListNode ListNode;
 
 ListNode* initNode(int val);
+linkedlist* initLL(int val);
+linkedlist* insertHead(linkedlist* ll, int val);
+linkedlist* insertTail(linkedlist* ll, int val);
+linkedlist* removeHead(linkedlist* ll);
+linkedlist* removeTail(linkedlist* ll);
 
-ListNode* addNode(ListNode* main_list, ListNode* add_node);
+void deleteLinkedList(linkedlist* ll);
+void printLinkedList(linkedlist* ll);
 
-ListNode* insertNode(ListNode* main_list, ListNode* insert_node);
 
 ListNode* initNode(int val) {
-    ListNode* init_node = (ListNode*) malloc(sizeof(ListNode));
-    init_node->val = val;
-    init_node->next = NULL;
-    return init_node;
+    ListNode* ll_node = malloc(sizeof(ListNode));
+    ll_node->val = val;
+    ll_node->next = NULL;
+    return ll_node;
 }
 
-ListNode* addNode(ListNode* main_list, ListNode* add_node) {
-    main_list->next = add_node;
-    ListNode* next_node = main_list->next;
-    return next_node;
+linkedlist* initLL(int val) {
+    linkedlist* ll = malloc(sizeof(linkedlist));
+    ll->head = initNode(val);
+    ll->tail = ll->head;
+    ll->size =  1;
+    return ll;
 }
+
+linkedlist* insertHead(linkedlist* ll, int val) {
+    ListNode* new_head = initNode(val);
+    new_head->next = ll->head;
+    ll->head = new_head;
+    ll->size++;
+    return ll;
+}
+
+linkedlist* insertTail(linkedlist* ll, int val) {
+    ListNode* new_tail = initNode(val);
+    (void) printf("%s %d- Current tail value %d\n", __func__, __LINE__, ll->tail->val);
+    ll->tail->next = new_tail;
+    ll->tail = new_tail;
+    ll->size++;
+    (void) printf("%s %d- Current tail value %d\n", __func__, __LINE__, ll->tail->val);
+    return ll;
+}
+
+linkedlist* removeHead(linkedlist* ll) {
+    ListNode* current_head = ll->head;
+    if(NULL == current_head) {
+/*         (void) printf("%s %d- head address %p \t ll size %d \n", __func__, __LINE__, current_head, ll->size);
+ */        return ll;
+    } else {
+/*         (void) printf("%s %d- head address %p \t ll size %d \n", __func__, __LINE__, current_head, ll->size); */
+        ListNode* new_head = current_head->next;
+        ll->head = new_head;
+        ll->size--;
+        (void) free(current_head);
+    }
+    return ll;
+}
+
+linkedlist* removeTail(linkedlist* ll) {
+    // roll through the linked list to find the previous node
+    ListNode* temp_node = ll->head;
+    
+    while(ll->tail != temp_node->next) {
+/*         (void) printf("%s %d- %d\n", __func__, __LINE__, temp_node->val); */
+        temp_node = temp_node->next;
+    }/* 
+    (void) printf("%s %d- current temp node value: %d\n", __func__, __LINE__, temp_node->val);
+    (void) printf("%s %d- current tail node value: %d\n", __func__, __LINE__, ll->tail->val);
+    (void) printf("%s %d- current temp node next value: %d\n", __func__, __LINE__, temp_node->next->val); */
+    ll->tail = temp_node;
+    (void) free(temp_node->next);
+    ll->size--;
+    return ll;
+}
+
+void deleteLinkedList(linkedlist* ll) {
+    while(0 != ll->size) {
+        ll = removeHead(ll);
+/*         (void) printf("remaining ll size: %d\n", ll->size); */
+    }
+    (void) free(ll);
+    return;
+}
+
+void printLinkedList(linkedList* ll) {
+    ListNode* current_node = ll->head;
+    for(uint16_t index = 0; index < ll->size; index++) {
+        (void) printf("%s %d- %d\n", __func__, __LINE__, current_node->val);
+        current_node = current_node->next;
+    }
+    return;
+}
+
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
 
@@ -48,25 +135,36 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
             break;
         };
         case 4 : {
-            ListNode* list3 = initNode(0);
+            int head_value = 0;
             if(list1->val > list2->val) { 
-                list3->val = list2->val;
+                head_value = list2->val;
                 list2 = list2->next;
             } else {
+                head_value = list1->val;
                 list1 = list1->next;
             }
+            linkedList* list3 = initLL(head_value);
             while((NULL != list1) && (NULL != list2)) {
                 if(list1->val <= list2->val) { 
-                    list3 = addNode(list3,list1);
+                    list3 = insertTail(list3,list1->val);
                     list1 = list1->next;
                 } else {
-                    list3 = addNode(list3, list2);
+                    list3 = insertTail(list3,list1->val);
                     list2 = list2->next;
                 }
             }
-            if(NULL == list2) { list3 = addNode(list3,list1); }
-            if(NULL == list1) { list3 = addNode(list3,list2); }
-            return list3;
+            if((NULL == list2) && (NULL != list1)) {
+                while(NULL != list1) {
+                    list3 = insertTail(list3, list1->val);
+                    list1 = list1->next;
+                } 
+            }
+            if(NULL == list1 && (NULL != list2)) {
+                while(NULL != list2) {
+                    list3 = insertTail(list3, list2->val);
+                    list2 = list2->next;
+                } 
+            }
             return list3;
             break;
         };
@@ -75,4 +173,4 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         };
     };
     return NULL;
-}
+} */
--- src/main.c
+++ src/main.c
@@ -4,7 +4,13 @@
 int main(int argument_count, char** argument_vector) {
   int error = 0;
   (void) printf("%s] %s) %d- argument_count: %d\n", __FILE__, __FUNCTION__, __LINE__, argument_count);
-  ListNode* list1 = initNode(1);
-  ListNode* list2 = initNode(int val)
+  linkedList* ll = initLL(1);
+  ll = insertHead(ll, 5);
+  ll = insertTail(ll, 7);
+  (void) printLinkedList(ll);
+  ll = removeTail(ll);
+  (void) printLinkedList(ll);
+  (void) deleteLinkedList(ll);
+
   return error;
 }
